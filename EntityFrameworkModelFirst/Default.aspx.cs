dad17e1f1ad50c6a270305a093b2c2ce068ba61d
yaaroslaw <yaaroslaw@gmail.com>
2017-04-04 14:53:11 +0300

    Realization of CRUD operations

--- EntityFrameworkModelFirst/Default.aspx.cs
+++ EntityFrameworkModelFirst/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,6 +17,53 @@ namespace EntityFrameworkModelFirst
         {
             dbContext = new Model1Container();
             ListView1.InsertItemPosition = InsertItemPosition.LastItem;
+        }
+
+
+
+
+        // Show all the customers
+        public IQueryable<Customer> GetCustomers()
+        {
+            // Use a LINQ Querry to get the info
+            return dbContext.Customers.AsQueryable<Customer>(); //lazy loading
+        }
+
+        // Edit Customers details
+        public void EditCustomer(int? customerId)
+        {
+            Customer customer = dbContext.Customers.FirstOrDefault(c => c.CustomerID == customerId);
+
+            if (customer != null && TryUpdateModel<Customer>(customer))
+            {
+                // With a help of EF, update the tables
+                dbContext.Entry<Customer>(customer).State = EntityState.Modified;
+                dbContext.SaveChanges();
+            }
+        }
+
+        // Delete a customer
+        public void DeleteCustomer()
+        {
+            Customer customer = new Customer();
+
+            if (TryUpdateModel<Customer>(customer))
+            {
+                dbContext.Entry<Customer>(customer).State = EntityState.Deleted;
+                dbContext.SaveChanges();
+            }
+        }
+
+        // Inserts a new Customer
+        public void InsertCustomer()
+        {
+            Customer customer = new Customer();
+
+            if (TryUpdateModel<Customer>(customer))
+            {
+                dbContext.Entry<Customer>(customer).State = EntityState.Added;
+                dbContext.SaveChanges();
+            }
         }
     }
 }
